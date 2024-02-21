48d8ae58b3fc35dcd94fcff211595222c019ce0b
Farid Huseynli <hseynli@list.ru>
2024-02-21 14:11:33 +0400

    New project added.

--- 01.EntityFramework/obj/Debug/net8.0/01.EntityFramework.AssemblyInfo.cs
+++ 01.EntityFramework/obj/Debug/net8.0/01.EntityFramework.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01.EntityFramework")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b6dd148310fc05f7a5854846e7c5dd2521d1991")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7940e1b2f9be9f8c27b1cc42a0c68b87374c09f")]
 [assembly: System.Reflection.AssemblyProductAttribute("01.EntityFramework")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01.EntityFramework")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ 06.MandatoryQueryString/QueryParameters.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+
+namespace MandatoryQueryStringParameters
+{
+    public class QueryParameters
+    {
+        public int Id { get; set; }
+
+        [BindRequired]
+        public int Number { get; set; }
+    }
+}
